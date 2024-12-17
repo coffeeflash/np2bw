a3ff83a0fb35a8c590e49c2816625d0ae765bc17
Paul Lardet <paul@MacBook-Air-de-Paul.local>
2024-12-17 12:07:50 +0100

    fix: Update base code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# np2bw
-Nextcloud Passwords to Bitwarden Vault
+# NP2W: Nextcloud Passwords to Bitwarden Vault
+
+Easily convert a Nextcloud Passwords (NP) database to a Bitwarden Vault (BW). 
+This script is based on [@monperrus](https://www.github.com/monperrus) work I found [here](https://gist.github.com/monperrus/c671817d53f6fc4bfe8d1773f28262d7).
+I wanted to update this script to make it work for my own migration from NP to BW. 
+
+# Set-up
+1. Clone repository:
+```
+https://github.com/godisopensource/np2bw.git
+```
+
+2. Ensure you have all necessary dependancies installed:
+```
+pip install csv json
+```
+3. Export your Nextcloud passwords as "Predefined csv" and put it in the same folder as this script.
+4. Put its path into `csv_file_path` variable
+5. The script will then convert your file to a `.json` file (which will be easier for BW to understand) and apply the correct formatting to your database, so it will be easily imported into BW.
+
+---
+Many thanks to [Martin Monperrus](https://www.github.com/monperrus) for having provided a base code and having been open to contributions. Feel free to contribute to this project too.
---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+# Basic migration from Nextcloud JSON (export) to Bitwarden JSON (import)
+# - folders are not supported
+# - type is only 'login' (no "card" "identity" or "secure note")
+#
+# Bitwarden JSON format doc: https://bitwarden.com/help/export-your-data/
+#
+# Authors: Martin Monperrus, Paul Lardet
+# Original URL: https://gist.github.com/monperrus/c671817d53f6fc4bfe8d1773f28262d7
+# New repository: 
+# License: Public domain
+
+import csv
+import json
+from datetime import datetime, timezone
+
+def csv_to_json(csv_file_path, json_file_path):
+    with open(csv_file_path, 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+
+    with open(json_file_path, 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, indent=4, ensure_ascii=False)
+
+csv_file_path = "/Users/paul/Downloads/np.csv" # Put CSV database from Nextcloud Passwords
+json_file_path = "/Users/paul/Downloads/nextcloudpasswords.json" # Json converted file will be here
+csv_to_json(csv_file_path, json_file_path)
+
+with open(json_file_path, "r", encoding='utf-8') as file:
+    kp = json.load(file)
+
+def entry_to_bw_json(entry):
+    current_time = datetime.now(timezone.utc).isoformat()
+    
+    return {
+        "passwordHistory": None,
+        "revisionDate": current_time,
+        "creationDate": current_time,
+        "deletedDate": None,
+        "id": entry.get("Id", ""),
+        "organizationId": None,
+        "folderId": entry.get("ID du dossier", None),
+        "type": 1,
+        "reprompt": 0,
+        "name": entry.get("Libéllé", ""),
+        "notes": entry.get("Notes") or None,
+        "favorite": entry.get("Favori", "").lower() == "vrai",
+        "login": {
+            "fido2Credentials": [],
+            "uris": [{"match": None, "uri": entry.get("Lien", "")}] if entry.get("Lien") else [],
+            "username": entry.get("Nom d’utilisateur", ""),
+            "password": entry.get("Mot de passe", ""),
+            "totp": None
+        },
+        "collectionIds": None
+    }
+
+def export(kp):
+    items = [entry_to_bw_json(i) for i in kp]
+    data = {
+        "encrypted": False,
+        "folders": [],
+        "items": items
+    }
+    
+    print(f'Imported {len(items)} elements')
+    
+    try:
+        with open('bitwarden_import.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
+        print(f"Successfully saved file to bitwarden_import.json")
+    except Exception as e:
+        print('An error occurred while trying to save json file:', e)
+
+export(kp)
+
